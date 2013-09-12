f65b0ba44b63a3f3baf1233b9a764e903f81d2dd
nicholas <njhanne@gmail.com>
2013-09-11 21:41:15 -0700

    finished initial commenting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   before_action :signed_in_user, only: [:edit, :update]
+  before_action :not_signed_in, only: [:new, :create]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
+
   def new
   	@user = User.new
   end
@@ -29,6 +31,7 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def edit
@@ -50,11 +53,8 @@ class UsersController < ApplicationController
     	params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in." unless signed_in?
-      end
+    def not_signed_in
+      redirect_to current_user if signed_in?
     end
 
     def correct_user
@@ -64,5 +64,5 @@ class UsersController < ApplicationController
 
     def admin_user
       redirect_to(root_url) unless current_user.admin?
-
+    end
 end
