4aa0a491e5c30ca21544c90d6f0f449d888a87da
Nicholas <njhanne@gmail.com>
2014-01-11 16:15:14 -0800

    remade for angular

---
+++ app/assets/javascripts/controllers/main/mainIndexCtrl.js
@@ -0,0 +1,13 @@
+this.IndexCtrl = function($scope) {
+  return $scope.data = {
+    posts: [
+      {
+        title: 'Post1',
+        contents: 'Hey, this site is gonna have cats, maps, and cats on maps.'
+      }, {
+        title: 'Post2',
+        contents: 'Cant wait for it to not be a shitty blog clone!'
+      }
+    ]
+  };
+};
